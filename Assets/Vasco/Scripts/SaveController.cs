838925c5ea3addc443f3d9b5392cd428a9053ccb
Vasco Almeida <l.vasco.m.a21@gmail.com>
2023-05-28 18:55:56 +0100

    Very basic save system using playerprefs

---
+++ Assets/Vasco/Scripts/SaveController.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveController : MonoBehaviour
+{
+    public static SaveController Instance;
+
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else if (Instance != this)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void SaveData(string name, int data)
+    {
+        PlayerPrefs.SetInt(name,data);
+    }
+
+    public void SaveData(string name, string data)
+    {
+        PlayerPrefs.SetString(name,data);
+    }
+
+    public void SaveData(string name, float data)
+    {
+        PlayerPrefs.SetFloat(name,data);   
+    }
+
+    public int LoadData(string name)
+    {
+        return PlayerPrefs.GetInt(name);
+    }
+
+    public string LoadDataString(string name)
+    {
+        return PlayerPrefs.GetString(name);
+    }
+
+    public float LoadDataFloat(string name)
+    {
+        return PlayerPrefs.GetFloat(name);
+    }
+}
