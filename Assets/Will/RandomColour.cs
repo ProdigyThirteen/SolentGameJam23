ab3ccd2d9a6ebb762ccd5dc838e558ff01345ee1
MrSwiftyMan <mihai.badea9@gmail.com>
2023-05-29 03:31:58 +0100

    experimenting with player segment colouring

--- Assets/Will/RandomColour.cs
+++ Assets/Will/RandomColour.cs
@@ -8,12 +8,22 @@ public class RandomColour : MonoBehaviour
     void Start()
     {
 
+        Color green = new Color(34.0f, 177.0f, 76.0f, 1.0f);
+        Color blue = new Color(76.0f, 226.0f, 255.0f, 1.0f);
+
         PlayerMovement playerScript = GameObject.FindWithTag("Player").GetComponent<PlayerMovement>();
 
         int maxSegments = GameManager.Instance.TotalSegments;
         int currentSegments = playerScript.getSegmentCount();
-        Debug.Log(GetComponent<SpriteRenderer>().color);
-        // GetComponent<SpriteRenderer>().color = new Color(0,255,0);
+
+        float percent = 0.5f;
+
+        
+
+        Color end_color = Color.Lerp(green, green, percent);
+
+        GetComponent<SpriteRenderer>().material.color = new Color(0,0.5f,0);
+        Debug.Log(GetComponent<SpriteRenderer>().material.color);
 
     }
 }
